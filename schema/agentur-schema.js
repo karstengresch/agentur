18fa7f346d7c4d67415dc75d1f871ef53c429ac6
Karsten Gresch <git-2016@gresch.io>
2019-01-23 00:12:27 +0100

    Schema work

--- schema/agentur-schema.js
+++ schema/agentur-schema.js
@@ -1,12 +1,42 @@
 export const typeDefs = gql`
+ 
+    # Agentur: Fact Types
+ 
     type Fact {
         id: ID
         
     }
     
     type ContinuousInformation {
+        id: ID
+        # check date type
+        date: Date
+    }
+    
+    type MediaData {
+        id: ID
+    }
+    
+    type Person {
+        id: ID
+    }
+    
+    type GeoData {
+        id: ID
+    }
+    
+    type Address {
+        id: ID
+    }
+    
+    type Citation {
         
     }
+    
+    # Agentur: Platform Types
+    
+    
+    # Queries
 
     type Query {
         users: [User]
